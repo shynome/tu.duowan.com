68790afa3300d14b120fc232d39530871250cfba
shynome <shynome@gmail.com>
2018-07-10 01:56:25 +0800

    xx

--- src/Article/ImageList.tsx
+++ src/Article/ImageList.tsx
@@ -3,8 +3,8 @@ import { Pic } from "../Api/image";
 
 export const ImageList:StatelessComponent<{list:Pic[]}> = (props)=>{
   let list = props.list.map(pic=>{
-    return <li key={ pic.pic_id } style={{margin:'0 0 3vw'}}>
-      {
+    return <li key={ pic.pic_id } style={{margin:'0 0 3vw', borderTop: '1px solid rgba(0,0,0,0.3)' }}>
+      {/* {
         pic.mp4_url
         ? <div className="video">
           <video src={pic.mp4_url} autoPlay loop width={pic.file_width} height={pic.file_height}></video>
@@ -12,10 +12,10 @@ export const ImageList:StatelessComponent<{list:Pic[]}> = (props)=>{
         :<div className="img">
           <img src={pic.source} style={{maxWidth:'100%'}}/>
         </div>
-      }
-      {/* <div className="img" >
+      } */}
+      <div className="img" >
         <img src={pic.source} style={{maxWidth:'100%'}} />
-      </div> */}
+      </div>
       <div className="add_intro">{ pic.add_intro }</div>
     </li>
   })
