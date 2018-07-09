b767ae06d510c7f044c853733c0ee9c3541e4881
shynome <shynome@gmail.com>
2018-07-10 01:47:52 +0800

    video 更清晰

--- src/Article/ImageList.tsx
+++ src/Article/ImageList.tsx
@@ -4,18 +4,18 @@ import { Pic } from "../Api/image";
 export const ImageList:StatelessComponent<{list:Pic[]}> = (props)=>{
   let list = props.list.map(pic=>{
     return <li key={ pic.pic_id } style={{margin:'0 0 3vw'}}>
-      {/* {
+      {
         pic.mp4_url
         ? <div className="video">
           <video src={pic.mp4_url} autoPlay loop width={pic.file_width} height={pic.file_height}></video>
         </div>
         :<div className="img">
-          <img src={pic.source} />
+          <img src={pic.source} style={{maxWidth:'100%'}}/>
         </div>
-      } */}
-      <div className="img" >
+      }
+      {/* <div className="img" >
         <img src={pic.source} style={{maxWidth:'100%'}} />
-      </div>
+      </div> */}
       <div className="add_intro">{ pic.add_intro }</div>
     </li>
   })
