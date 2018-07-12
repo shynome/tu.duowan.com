495c3cf64c6518e3786d8532a7c86cecedc00b89
shynome <shynome@gmail.com>
2018-07-12 13:29:17 +0800

    添加剩下的几个

--- src/Pages/data.ts
+++ src/Pages/data.ts
@@ -3,4 +3,8 @@ export const Name2Tag:{ [key:string]:{ tag:number, name:string } } = {
   'tucao':    { name: '吐槽', tag:15203, },
   'jiongtu':  { name: '囧图', tag:56, },
   'gif':      { name: 'GIF', tag:9031, },
+  'meinv':    { name: '软妹', tag:9031, },
+  'Trivia':   { name: '冷知识', tag:20721, },
+  'cold_joke':{ name: '冷笑话', tag:25047, },
+  'photos':   { name: '组图', tag:26513, },
 }
