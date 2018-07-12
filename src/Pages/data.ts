f26042a2101ed6ec24d5e5a60f04cbc38b3c69f1
shynome <shynome@gmail.com>
2018-07-12 13:35:44 +0800

    修改为小写的

--- src/Pages/data.ts
+++ src/Pages/data.ts
@@ -4,7 +4,7 @@ export const Name2Tag:{ [key:string]:{ tag:number, name:string } } = {
   'jiongtu':  { name: '囧图', tag:56, },
   'gif':      { name: 'GIF', tag:9031, },
   'meinv':    { name: '软妹', tag:9031, },
-  'Trivia':   { name: '冷知识', tag:20721, },
+  'trivia':   { name: '冷知识', tag:20721, },
   'cold_joke':{ name: '冷笑话', tag:25047, },
   'photos':   { name: '组图', tag:26513, },
 }
