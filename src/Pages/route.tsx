b3f208c1c6ea9ba0682492b294f7d686eb9f6569
shynome <shynome@gmail.com>
2018-07-13 16:30:52 +0800

    添加页数记录

--- src/Pages/route.tsx
+++ src/Pages/route.tsx
@@ -1,15 +1,34 @@
-import { createElement as h } from "react";
-import { Redirect } from "react-router";
+import { createElement as h, StatelessComponent } from "react";
+import { Redirect, RouteComponentProps } from "react-router";
 
 import { AsyncRoute } from "../Components";
 
-const Page = AsyncRoute(()=>import('.').then(t=>t.Pages))
+const page = AsyncRoute(()=>import('.').then(({ Pages })=>{
+  return (props:RouteComponentProps<{ page:string }>)=>{
+
+    let path = '/'+props.match.path.split('/')[1]
+    localStorage.setItem(path,props.match.params.page)
+    
+    return <Pages {...props}/>
+
+  }
+}))
+
+
+const redirect:StatelessComponent<RouteComponentProps<any>> = (props)=>{
+
+  const lastViewPage = localStorage.getItem(props.match.path) || 1
+  
+  return <Redirect to={ `${props.match.path}/${lastViewPage}` } />
+
+}
+
 import { Name2Tag } from "./data";
 export const PagesRoute = Object.keys(Name2Tag)
 .map(name=>{
   return [
-    [ `/${name}`, ()=><Redirect to={ `/${name}/1` } /> ],
-    [ `/${name}/:page`, Page ]
+    [ `/${name}`, redirect ],
+    [ `/${name}/:page`, page ]
   ] as [ string, any ][]
 })
 .reduce((p,v)=>p.concat(v),[])
